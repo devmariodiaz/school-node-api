5fc8311b112a1dff5356e439417775a2fa8edf7d
Mario Diaz <dev.mariodiaz@gmail.com>
2021-06-02 14:55:21 -0500

    Adding helmet package

--- middleware/server.js
+++ middleware/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const helmet = require('helmet');
 const routes = require('../routes/index.routes')
 const app = express();
 
 const start = (port) => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
-    // app.use(express.static("public"));
+    app.use(express.static("public"));
 
-    // app.use("/api", routes);
+    app.use(helmet()); // Helmet help us to protect the app from some known vulnerabilities
 
     app.listen (port, () => {
         console.log(`Server has started listening in ${port} port`);
